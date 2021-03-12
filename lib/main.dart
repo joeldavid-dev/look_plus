824967e381e27fec90b91268e78b5d8993c24ac4
Joel David C <jdcruzzamora@gmail.com>
2021-03-11 22:40:33 -0600

    Problema con status bar oscura solucionado

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,31 @@ import 'package:look_plus/screens/home.dart';
 import 'package:look_plus/screens/peliculaInfoScreen.dart';
 import 'package:look_plus/screens/search_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: colorFondoApp,
+  ));
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      systemNavigationBarColor: colorFondoApp,
-    ));
-
     return MaterialApp(
       title: tituloApp,
       theme: ThemeData(
-        floatingActionButtonTheme:
-            FloatingActionButtonThemeData(backgroundColor: colorAcento),
+        //El brillo del tema en general es oscuro. Ayuda a que el status bar no se oscurezca.
+        brightness: Brightness.dark,
         appBarTheme: AppBarTheme(color: colorAcento),
         scaffoldBackgroundColor: colorFondoApp,
         textTheme: Theme.of(context)
             .textTheme
             .apply(fontFamily: 'Varela', bodyColor: colorLetras),
-        primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        accentColor: colorAcento,
       ),
       debugShowCheckedModeBanner: false,
       initialRoute: 'home',
